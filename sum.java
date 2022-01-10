9545f8609a885d22396350c7e27d0cbc8eecf408
Vinayak Arora <vinayak270603@gmail.com>
2022-01-10 13:18:06 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,15 @@
+public class sum {
+    public static void main(String[] args) {
+        
+        int a=5;
+        int b=6;
+        int sum;
+        sum=a+b;
+        System.out.println(sum);
+       
+    {
+       
+   }
+        
+    }
+}
