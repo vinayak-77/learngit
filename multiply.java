b163d3c537a61b1876fe3ebb2ce0c001fb9a684b
Vinayak Arora <vinayak270603@gmail.com>
2022-01-10 13:36:03 +0530

    added multiply

---
+++ multiply.java
@@ -0,0 +1,11 @@
+public class multiply {
+    public static void main(String[] args) {
+        int a=8;
+        int b=9;
+        int pro;
+        pro=a*b;
+        System.out.println(pro);
+        
+    }
+    
+}
