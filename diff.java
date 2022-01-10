6a3a732e42be62bda4cb50f0ae995b3bb6e91798
Vinayak Arora <vinayak270603@gmail.com>
2022-01-10 13:28:01 +0530

    made change to sum.java

--- diff.java
+++ diff.java
@@ -1,4 +1,14 @@
 public class diff {
+    public static  void main(String[] args) {
+        int a=7;
+        int b=5;
+        int diff;
+        diff=a-b;
+        System.out.println(diff);
+        
     
+        
+    }
+
     
 }
--- sum.java
+++ sum.java
@@ -1,11 +1,11 @@
 public class sum {
     public static void main(String[] args) {
         
-        int a=5;
+        int a=7;
         int b=6;
         int sum;
         sum=a+b;
-        System.out.println(sum);
+        System.out.println("The sum is " +(sum));
        
     {
        
